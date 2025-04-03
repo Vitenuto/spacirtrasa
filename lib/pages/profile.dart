76e67b02f17e7055e2eb6a322441b40987b1e785
Vit Nakladal <vit.nakladal@seacomp.cz>
2025-04-03 12:09:35 +0200

    FIX: Signing out/in
    - Fixed signing in after user signed out to be able to choose from multiple google accounts again
    
    - Fixed routing to do not show the welcome page again after user signed in

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,28 +1,41 @@
+import 'package:async_button_handler/async_button_handler.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spacirtrasa/providers/auth_user.dart';
 
 import '../services/auth_service.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends ConsumerWidget {
   static const route = "/profile";
 
   const ProfilePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("Profile Page"),
-          ElevatedButton(
-            onPressed: () async {
-              await AuthService.signOut();
-            },
-            child: Text("profile.sign-out").tr(),
-          ),
+          _buildSignInOutButton(ref),
         ],
       ),
+    );
+  }
+
+  AsyncButtonHandler _buildSignInOutButton(WidgetRef ref) {
+    final user = ref.watch(authUserProvider);
+    final isSignedIn = user != null;
+
+    return AsyncButtonHandler(
+      onPressed: () async => isSignedIn ? await AuthService.signOut() : await AuthService.signInWithGoogle(),
+      loadingChild: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: CircularProgressIndicator(),
+      ),
+      widget: isSignedIn ? Text("profile.sign-out").tr() : Text("profile.sign-in").tr(),
     );
   }
 }
