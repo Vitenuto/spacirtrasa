ae8bd91cbb379148ca760a7d2803037c26d26bcf
Vit Nakladal <vit.nakladal@seacomp.cz>
2025-04-04 13:11:13 +0200

    FEAT: Profile page extended
    - Added buttons and user information

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -14,28 +14,80 @@ class ProfilePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(authUserProvider);
+
     return Center(
       child: Column(
+        spacing: 8,
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [_buildProfileHeader(user), if (user != null) _buildButtons(user)],
+      ),
+    );
+  }
+
+  Widget _buildProfileHeader(final User? user) {
+    return IntrinsicWidth(
+      child: Column(
         children: [
-          Text("Profile Page"),
-          _buildSignInOutButton(ref),
+          user == null
+              ? SizedBox()
+              : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundColor: Colors.grey[300], // Placeholder background
+                    backgroundImage:
+                        user?.photoURL != null
+                            ? NetworkImage(user!.photoURL!) // Load image if available
+                            : null, // No image if null
+                    child:
+                        user?.photoURL == null
+                            ? Icon(Icons.person, size: 40, color: Colors.white) // Default icon
+                            : null,
+                  ),
+                  SizedBox(width: 8),
+                  Text(user.displayName ?? user.email ?? user.uid, style: TextStyle(fontSize: 24)),
+                ],
+              ),
+          SizedBox(height: 8),
+          _buildSignInOutButton(user != null),
         ],
       ),
     );
   }
 
-  AsyncButtonHandler _buildSignInOutButton(WidgetRef ref) {
-    final user = ref.watch(authUserProvider);
-    final isSignedIn = user != null;
-
+  AsyncButtonHandler _buildSignInOutButton(final bool isSignedIn) {
     return AsyncButtonHandler(
-      onPressed: () async => isSignedIn ? await AuthService.signOut() : await AuthService.signInWithGoogle(),
-      loadingChild: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: CircularProgressIndicator(),
-      ),
+      style: OutlinedButton.styleFrom(minimumSize: const Size(double.infinity, 48)),
+      onPressed:
+          () async =>
+              isSignedIn ? await AuthService.signOut() : await AuthService.signInWithGoogle(),
+      loadingChild: CircularProgressIndicator(),
       widget: isSignedIn ? Text("profile.sign-out").tr() : Text("profile.sign-in").tr(),
     );
   }
+
+  Widget _buildButtons(final User user) {
+    return IntrinsicWidth(
+      child: Column(
+        spacing: 8,
+        children: [
+          SizedBox(width:256),
+          bodyButton("profile.my-favorites".tr(), () => {}),
+          bodyButton("profile.my-ratings".tr(), () => {}),
+          bodyButton("profile.my-notes".tr(), () => {}),
+          bodyButton("profile.my-completed-paths".tr(), () => {}),
+          if (user != null) ...[
+            const Text("profile.admin-section", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)).tr(),
+
+            bodyButton("profile.manage-poi".tr(), () => {}),
+            bodyButton("profile.manage-paths".tr(), () => {}),
+          ],
+        ],
+      ),
+    );
+  }
+
+  OutlinedButton bodyButton(final String text, final VoidCallback onPressed) => OutlinedButton(style: OutlinedButton.styleFrom(minimumSize: const Size(double.infinity, 42)), onPressed: onPressed, child: Text(text));
 }
