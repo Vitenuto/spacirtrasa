45917787a8cb59f52553785448b92e8c35147662
Vit Nakladal <vit.nakladal@seacomp.cz>
2025-05-08 13:05:26 +0200

    FIX: Snapping functionality of lists is fixed
    - Map is automatically aligned by currently selectedPoi/Trail
    
    - Bottom list is automatically aligned by selected Poi/Trail

--- lib/pages/home/snap_list.dart
+++ lib/pages/home/snap_list.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
-import 'package:spacirtrasa/models/map_entity/poi/poi.dart';
 import 'package:spacirtrasa/models/map_entity/poi/poi_with_distance.dart';
 import 'package:spacirtrasa/pages/home/animated_poi_tile.dart';
+import 'package:spacirtrasa/providers/expanded.dart';
 import 'package:spacirtrasa/providers/map_entity/poi/selected_poi.dart';
 import 'package:spacirtrasa/providers/map_entity/poi/sorted_poi.dart';
 import 'package:spacirtrasa/utils/constants.dart';
+import 'package:spacirtrasa/utils/utils.dart';
+import 'package:spacirtrasa/widgets/snap_list_helper.dart';
 
 class SnapList extends ConsumerStatefulWidget {
   final bool isExpanded;
@@ -21,7 +23,6 @@ class _SnapListState extends ConsumerState<SnapList> {
   final log = Logger();
   final ScrollController _scrollController = ScrollController();
 
-  double get _fullItemHeight => itemListHeight + (itemListPadding * 2);
   late List<PoiWithDistance> _poisWithDistance;
 
   @override
@@ -30,24 +31,21 @@ class _SnapListState extends ConsumerState<SnapList> {
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _scrollController.addListener(_setSelected);
-      _scrollController.position.isScrollingNotifier.addListener(() => _handleSnap(ref.read(selectedPoiProvider)));
-      final initialSelected = ref.read(selectedPoiProvider);
-      _handleSnap(initialSelected);
+      _scrollController.position.isScrollingNotifier.addListener(_onScrollChange);
+      _snapToSelected();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     _poisWithDistance = ref.watch(sortedPoiProvider);
-    if (!widget.isExpanded) {
-      ref.listen(selectedPoiProvider, ((_, selectedPoi) => _handleSnap(selectedPoi)));
-    }
+    ref.listen(expandedProvider, (_, isExpanded) => _onExpanded(isExpanded));
 
     return Padding(
       padding: const EdgeInsets.only(top: 4.0),
       child: ListView.builder(
         padding: EdgeInsets.all(8),
-        cacheExtent: _fullItemHeight * 15,
+        cacheExtent: fullItemHeight * 15,
         controller: _scrollController,
         itemCount: _poisWithDistance.length + 2,
         // +2 for the title and spacer
@@ -62,7 +60,7 @@ class _SnapListState extends ConsumerState<SnapList> {
             );
           } else if (index == _poisWithDistance.length + 1) {
             // Spacer to allow last item to reach top
-            return SizedBox(height: _fullItemHeight * 5);
+            return SizedBox(height: fullItemHeight * 5);
           }
 
           final poiWithDistance = _poisWithDistance[index - 1];
@@ -72,44 +70,39 @@ class _SnapListState extends ConsumerState<SnapList> {
     );
   }
 
-  Future<void> _setSelected() async {
+  void _onExpanded(final bool isExpanded) {
+    if (!isExpanded) _snapToSelected();
+  }
+
+  void _setSelected() async {
     if (widget.isExpanded) return;
 
     final offset = _scrollController.offset;
-    var index = (offset / _fullItemHeight).round();
-    if (index <= 1) index = 1;
-    if (ref.read(selectedPoiProvider)?.id == _poisWithDistance[index - 1].poi.id) {
+    var index = (offset / fullItemHeight).round() - 1; // -1 to account for the title
+    index = index.clamp(0, _poisWithDistance.length - 1);
+    final selectedPoi = _poisWithDistance[index].poi;
+    if (ref.read(selectedPoiProvider)?.id == selectedPoi.id) {
       return; // Skip if the same item is selected
     }
-    ref.read(selectedPoiProvider.notifier).setSelected(_poisWithDistance[index - 1].poi);
+    ref.read(selectedPoiProvider.notifier).setSelected(selectedPoi);
   }
 
-  Future<void> _handleSnap(Poi? selectedPoi) async {
-    if (_scrollController.position.isScrollingNotifier.value) return;
-    log.d('Snapping to POI: ${selectedPoi?.title}');
+  void _onScrollChange() async {
+    if (widget.isExpanded || _scrollController.position.isScrollingNotifier.value) return;
+    _snapToSelected();
+  }
 
+  void _snapToSelected() {
+    final selectedPoi = ref.read(selectedPoiProvider);
     if (selectedPoi == null) return;
 
     final poiIndex = _poisWithDistance.indexWhere(
       (poiWithDistance) => poiWithDistance.poi.id == selectedPoi.id,
     );
-    final elementIndex = poiIndex + 1;
-    final snappedOffset = (elementIndex * _fullItemHeight).clamp(
-      0.0,
-      _scrollController.position.maxScrollExtent,
-    );
+    final listIndex = poiIndex + 1; // +1 to account for the title
+    if (listIndex == 1) return; // Skip snapping to the first item
 
-    if (elementIndex == 1 || snappedOffset == _scrollController.offset) {
-      return; // Skip snapping to the first item
-    }
-
-    log.t(
-      'With offset: ${_scrollController.offset} and calculated snappedOffset: $snappedOffset, snapping to POI: ${selectedPoi.title}',
-    );
-    await _scrollController.animateTo(
-      snappedOffset,
-      duration: kThemeAnimationDuration,
-      curve: Curves.easeOut,
-    );
+    log.t('Snapping to: ${selectedPoi.title}');
+    handleSnap(_scrollController, listIndex);
   }
 }
---
+++ lib/widgets/snap_list_helper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:spacirtrasa/utils/utils.dart';
+
+Future<void> handleSnap(final ScrollController scrollCtrl, final int listIndex) async {
+  final snappedOffset = (listIndex * fullItemHeight).clamp(
+    0.0,
+    scrollCtrl.position.maxScrollExtent,
+  );
+
+  if (snappedOffset == scrollCtrl.offset) return; // Skip snapping to the same item
+
+  await scrollCtrl.animateTo(
+    snappedOffset,
+    duration: kThemeAnimationDuration,
+    curve: Curves.easeOut,
+  );
+}
