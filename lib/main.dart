93efba6b7e3c598a7faf43f2a6df5e76bc185793
Vit Nakladal <vit.nakladal@seacomp.cz>
2025-05-31 15:17:13 +0200

    CHORE: Removed rotation possibility

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
@@ -12,6 +13,10 @@ import 'firebase_options.dart';
 void main() async {
   Logger.level = Level.all;
   await dotenv.load(fileName: ".env");
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await EasyLocalization.ensureInitialized();
@@ -40,10 +45,7 @@ class App extends StatelessWidget {
       title: 'Špacírtrasa vokolo Ořechova',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.yellow,
-          brightness: Brightness.light,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow, brightness: Brightness.light),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routerConfig: RouteService.router,
