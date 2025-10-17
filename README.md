e7089b9f4a8ae4f7f00af2678763ed4398ff77de
Vit Nakladal <vit.nakladal@seacomp.cz>
2025-10-17 19:54:09 +0200

    FEAT: AppCheck security implemented

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Procházky vokolo Ořechova
 ## Popisek
 
 Aplikace pro jednoduché najití tras a zajímavostí okolo obce Ořechov u Brna.
+
+
+## TODO: Jak aplikaci sestavit
+
+1. Konfigurace Firebase
+2. AppCheck klíč se zobrazí v konzoli - je nutné jej přidat do Firebase aplikace.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,6 +22,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await EasyLocalization.ensureInitialized();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+  );
 
   runApp(
     EasyLocalization(
